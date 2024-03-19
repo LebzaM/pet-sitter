c508ea5983f937daba730c47bddbd1ea8f70be88
LebzaM <lebomariri505@gmail.com>
2024-03-19 17:33:37 +0200

    cors refere and header for deployment

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const port =5000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'https://pet-owner.vercel.app'
+    origin: 'https://pet-owner.vercel.app/'
   }));
 
 
